25c575930314bdec6606ddef6eb7babfcb939e11
ompiepy <66164291+ompiepy@users.noreply.github.com>
2020-05-30 16:21:01 +0545

    Add files via upload

---
+++ rainfall.py
@@ -0,0 +1,26 @@
+print('Is raining? (Y)es or (N)o')
+answer = input()
+if answer == 'N':
+	print('Go outside.')
+elif answer == 'Y':
+	print('Have umbrella? (Y)es or (N)o')
+	answer2 = input()
+	if answer2 == 'Y':
+		print('Go outside.')
+	elif answer2 == 'N':
+		print('Wait a while.')
+		print('Is raining? (Y)es or (N)o')
+		answer3 = input()
+		while answer3 == 'Y':
+			print('Wait a while.')
+			print('Is raining? (Y)es or (N)o')
+			answer3 = input()
+		print('Go outside.')
+else:
+	print("I can't understand you.Type 'Y' for yes and 'N' or No.")
+print('===============')
+print('Exiting program')
+
+	
+
+
